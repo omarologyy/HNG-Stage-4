d4ce4b9f54d90cd3b374dbb2624e9ffde4cb1723
Umar Sulaiman <akoredeumar8@gmail.com>
2025-11-12 17:04:32 +0100

    fix: hosting issues

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,18 @@
 module.exports = function (api) {
-  api.cache(false);
+  api.cache(true);
   return {
     presets: ["babel-preset-expo"],
     plugins: [
       [
         "module:react-native-dotenv",
         {
-          envName: "APP_ENV",
           moduleName: "@env",
           path: ".env",
+          safe: false,
+          allowUndefined: false,
         },
       ],
+      "react-native-reanimated/plugin",
     ],
   };
 };
--- declarations.d.ts
+++ declarations.d.ts
@@ -5,3 +5,4 @@ declare module "*.svg";
 // declarations.d.ts
 declare module "expo-linear-gradient";
 declare module "firebase/auth/react-native";
+declare module "firebase/auth";
